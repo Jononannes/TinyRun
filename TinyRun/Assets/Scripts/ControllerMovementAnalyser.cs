6555910b8cf4bf8209bf13fd3c1652a4c23992b4
Johannes Kvernes <johannes.kvernes@gmail.com>
2022-09-27 22:32:01 +0200

    Play footstep sound when swinging arms

--- TinyRun/Assets/Scripts/ControllerMovementAnalyser.cs
+++ TinyRun/Assets/Scripts/ControllerMovementAnalyser.cs
@@ -6,30 +6,45 @@ public class ControllerMovementAnalyser : MonoBehaviour {
 
     public ControllerTracker rightController;
     public ControllerTracker leftController;
-    public Transform environment;
+    public Transform player;
+    public AudioClip footstepClip;
 
     // configurable settings
     [Range(0f, 5f)]
-    public float minimumArmMovementForRun = 1f;
-    [Range(0f, 5f)]
-    public float minimumEnvironmentMovement = 0f;
+    public float minimumArmMovementForRun = 0.5f;
     [Range(0f, 5f)]
     public float armToEnvironmentMovementScaling = 0.25f;
-    
 
-    // Start is called before the first frame update
-    void Start() {
 
+    private AudioSource audioSource;
+    private bool lastRightArmSwingForward = false;
+
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
     }
+
+
 
     // Update is called once per frame
     void Update() {
-        if (rightController.GetVelocity().z > 0 && leftController.GetVelocity().z < 0 ||
-            rightController.GetVelocity().z < 0 && leftController.GetVelocity().z > 0) {
+        if (rightController.GetVelocity().z > minimumArmMovementForRun && leftController.GetVelocity().z < -minimumArmMovementForRun ||
+            rightController.GetVelocity().z < -minimumArmMovementForRun && leftController.GetVelocity().z > minimumArmMovementForRun) {
+
             float totalZMovement = Mathf.Abs(rightController.GetVelocity().z) + Mathf.Abs(leftController.GetVelocity().z);
             if (totalZMovement > minimumArmMovementForRun) {
-                float environmentMovement = (totalZMovement - minimumArmMovementForRun) * armToEnvironmentMovementScaling + minimumEnvironmentMovement;
-                environment.transform.position -=  new Vector3(0f, 0f, environmentMovement * Time.deltaTime);
+                float environmentMovement = (totalZMovement - minimumArmMovementForRun) * armToEnvironmentMovementScaling;
+                player.position +=  new Vector3(0f, 0f, environmentMovement * Time.deltaTime);
+
+                bool rightArmSwingForward = rightController.GetVelocity().z > 0;
+
+                if ((rightArmSwingForward && !lastRightArmSwingForward) ||
+                    (!rightArmSwingForward && lastRightArmSwingForward)) {
+                    audioSource.pitch = Random.Range(0.8f, 1.25f);
+                    audioSource.Play();
+                }
+
+                lastRightArmSwingForward = rightArmSwingForward;
             }
         }
     }
