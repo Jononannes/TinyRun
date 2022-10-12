3707050051cd6633f35a7c7aa97f5fc5fee8b7a7
Johannes Kvernes <johannes.kvernes@gmail.com>
2022-10-12 22:53:06 +0200

    Spider plays sound on footstep

--- TinyRun/Assets/Scripts/Spider/Spider.cs
+++ TinyRun/Assets/Scripts/Spider/Spider.cs
@@ -14,13 +14,17 @@ public class Spider : MonoBehaviour {
     private Vector3[] points;
     private Vector3[] oldPoints;
     private float[] legAnimationLerps;
+    private float[] prevLegAnimationLerps;
     private Vector3 prevPosition;
+    private AudioSource audioSource;
 
 
     void Start() {
+        audioSource = GetComponent<AudioSource>();
         points = new Vector3[iks.Length];
         oldPoints = new Vector3[iks.Length];
         legAnimationLerps = new float[iks.Length];
+        prevLegAnimationLerps = new float[iks.Length];
         prevPosition = transform.position;
 
         for (int i = 0; i < iks.Length; i++) {
@@ -85,7 +89,14 @@ public class Spider : MonoBehaviour {
                     }
                 } else {
                     legAnimationLerps[i] = Mathf.Clamp(legAnimationLerps[i] + animationSpeed * Time.deltaTime, 0, 1);
+
+                    if (legAnimationLerps[i] == 1 && prevLegAnimationLerps[i] < 1) {
+                        audioSource.pitch = Random.Range(0.8f, 1.25f);
+                        audioSource.Play();
+                    }
                 }
+
+                prevLegAnimationLerps[i] = legAnimationLerps[i];
 
                 // make the leg follow the position of where the foot should be
                 iks[i].follow(legAnimationLerps[i] * points[i] + (1 - legAnimationLerps[i]) * oldPoints[i]);
